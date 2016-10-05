83cfbfbf0864eec9fd93c139c121f25fb4407e67
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2016-10-05 09:38:47 +0200

    Last commit of the training

--- app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/main/MainActivity.java
+++ app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/main/MainActivity.java
@@ -1,76 +1,18 @@
 package com.android2ee.formation.juilmmxvi.teleric.view.main;
 
-import android.animation.AnimatorInflater;
-import android.animation.AnimatorSet;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.android2ee.formation.juilmmxvi.teleric.R;
-import com.android2ee.formation.juilmmxvi.teleric.model.Human;
 import com.android2ee.formation.juilmmxvi.teleric.view.generic.MotherActivity;
-import com.android2ee.formation.juilmmxvi.teleric.view.main.adapter.HumanRecyclerAdapter;
-import com.android2ee.formation.juilmmxvi.teleric.view.main.adapter.OnItemClickedRecyclerView;
 
-import java.util.ArrayList;
-
-public class MainActivity extends MotherActivity implements OnItemClickedRecyclerView {
+public class MainActivity extends MotherActivity {
     private static final String TAG = "MainActivity";
-    /***********************************************************
-     * Constant
-     **********************************************************/
-    public static final String EDT = "EDT";
-    public static final String RES = "RES";
-    /***********************************************************
-     *  Attributes
-     **********************************************************/
-    /**
-     * The editText to write message
-     */
-    private EditText edtMessage;
-    /**
-     * The button to add the message in  the result area
-     */
-    private Button btnAdd;
-    /**
-     * The Result area
-     */
-    private RecyclerView rcvResult;
-    /**
-     * The arrayAdapter == the model of the list view
-     */
-    private HumanRecyclerAdapter humanRecyclerAdapter;
-    /**
-     * The list of messages to display
-     */
-    private ArrayList<Human> humen;
-    /**
-     * HoneyComb or not  ?o?
-     */
-    private boolean isHoneyComb;
 
-    /**
-     * Animator Set for animation postHC
-     */
-    AnimatorSet animatorSet;
-    /**
-     * Animator Set for animation preHC
-     */
-    Animation animation;
 
     /***********************************************************
      * Managing LifeCycle
@@ -82,47 +24,7 @@ public class MainActivity extends MotherActivity implements OnItemClickedRecycle
         super.onCreate(savedInstanceState);
         //instantiate the view
         setContentView(R.layout.activity_main);
-        //instantiate graphical components
-        edtMessage = (EditText) findViewById(R.id.edtMessage);
-        btnAdd = (Button) findViewById(R.id.btnAdd);
-        rcvResult = (RecyclerView) findViewById(R.id.rcvResult);
-        isHoneyComb=getResources().getBoolean(R.bool.isHC);
-        //instanciate animation
-        if(isHoneyComb){
-            animatorSet= (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.btnadd_pushed_anim);
-            animatorSet.setTarget(btnAdd);
-        }else{
-            animation= AnimationUtils.loadAnimation(this,R.anim.btn_add_pushed);
-        }
-        //to avoid selector to be drawn like a big rectancle
-        if (savedInstanceState != null) {
-            humen = savedInstanceState.getParcelableArrayList(RES);
-        } else {
-            humen = new ArrayList<>();
-            for(int i=0;i<2;i++){
-                humen.add(new Human("Item "+i,i));
-            }
-        }
-        humanRecyclerAdapter = new HumanRecyclerAdapter(humen, this) {
-            /**
-             * The item is clicked please do something
-             *
-             * @param position
-             */
-            @Override
-            public void itemClicked(int position) {
-                MainActivity.this.itemClicked(position);
-            }
-        };
-        rcvResult.setLayoutManager(new LinearLayoutManager(this));
-        rcvResult.setAdapter(humanRecyclerAdapter);
-        //implement the listeners
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addMessageInResult();
-            }
-        });
+
     }
 
 
@@ -130,20 +32,13 @@ public class MainActivity extends MotherActivity implements OnItemClickedRecycle
     protected void onSaveInstanceState(Bundle outState) {
         Log.d(TAG, "onSaveInstanceState() called with: " + "outState = [" + outState + "]");
         super.onSaveInstanceState(outState);
-        outState.putString(EDT, edtMessage.getText().toString());
-        outState.putParcelableArrayList(RES, humen);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         Log.d(TAG, "onRestoreInstanceState() called with: " + "savedInstanceState = [" + savedInstanceState + "]");
         super.onRestoreInstanceState(savedInstanceState);
-        edtMessage.setText(savedInstanceState.getString(EDT));
-//        ArrayList<String> temp=savedInstanceState.getStringArrayList(RES);
-//        for (int i = 0; i < temp.size(); i++) {
-//            messages.add(temp.get(i));
-//        }
-//        arrayAdapter.notifyDataSetChanged();
+
     }
 
     /***********************************************************
@@ -167,23 +62,8 @@ public class MainActivity extends MotherActivity implements OnItemClickedRecycle
      * Create the AlertDialog
      */
     private Dialog buildDialog(){
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
-        builder.setMessage(getString(R.string.alertdialog_message,messageTemp));
-        builder.setPositiveButton(getString(R.string.alertdialog_ok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        copyMessageInEdt();
-                    }
-                });
-        builder.setNegativeButton(getString(R.string.alertdialog_nok),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        abordCopy();
-                    }
-                });
-        return builder.create();
+
+        return null;
     }
 
     /***********************************************************
@@ -191,87 +71,14 @@ public class MainActivity extends MotherActivity implements OnItemClickedRecycle
      **********************************************************/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //create the menu
-        MenuInflater menuInflater=getMenuInflater();
-        menuInflater.inflate(R.menu.main_menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.menu_deleteall:
-                deleteAll();
-                return true;
-            case R.id.menu_addall:
-                addAll();
-                return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
-    /***********************************************************
-     *  Business Methods
-     **********************************************************/
-    String messageTemp;
-    Human humanTemp;
-    /**
-     * Add the message from the editText in the Result area
-     */
-    @SuppressLint("NewApi")
-    private void addMessageInResult() {
-        if(isHoneyComb){
-            animatorSet.start();
-        }else{
-            btnAdd.startAnimation(animation);
-        }
-        messageTemp = edtMessage.getText().toString();
-        //first way
-//        messages.add(message);
-//        arrayAdapter.notifyDataSetChanged();
-        //second
-        humanRecyclerAdapter.add(0,new Human(messageTemp, humen.size()));
-        edtMessage.setText("");
-    }
 
-    /**
-     * Copy the item selected into the editText
-     * @param position
-     */
-    public void itemClicked(int position){
-        messageTemp=humanRecyclerAdapter.getItem(position).getMessage();
-        showDialog(AlertDialogUniqueID);
-    }
-
-    /**
-     * Copythe value of message in the EditText
-     */
-    private void copyMessageInEdt(){
-        edtMessage.setText(messageTemp);
-    }
-
-    /**
-     * Abord copy
-     */
-    private void abordCopy(){
-        Toast.makeText(this, R.string.toast_abordcopy,Toast.LENGTH_LONG).show();
-    }
-
-    /**
-     * Delete all the item
-     */
-    public void deleteAll(){
-        humanRecyclerAdapter.deleteAll();
-    }
-
-    /**
-     * Add a bunch of people
-     */
-    public void addAll(){
-        int size= humanRecyclerAdapter.getItemCount();
-        for(int i=size;i<size+2;i++){
-            humen.add(new Human("Item "+i,i));
-        }
-        humanRecyclerAdapter.notifyItemRangeInserted(size,2);
-    }
 }
---
+++ app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/main/main_fragment/MainFragment.java
@@ -0,0 +1,335 @@
+/**
+ * <ul>
+ * <li>MainFragment</li>
+ * <li>com.android2ee.formation.juilmmxvi.teleric.view.main.main_fragment</li>
+ * <li>22/07/2016</li>
+ * <p/>
+ * <li>======================================================</li>
+ * <p/>
+ * <li>Projet : Mathias Seguy Project</li>
+ * <li>Produit par MSE.</li>
+ * <p/>
+ * /**
+ * <ul>
+ * Android Tutorial, An <strong>Android2EE</strong>'s project.</br>
+ * Produced by <strong>Dr. Mathias SEGUY</strong>.</br>
+ * Delivered by <strong>http://android2ee.com/</strong></br>
+ * Belongs to <strong>Mathias Seguy</strong></br>
+ * ***************************************************************************************************************</br>
+ * This code is free for any usage but can't be distribute.</br>
+ * The distribution is reserved to the site <strong>http://android2ee.com</strong>.</br>
+ * The intelectual property belongs to <strong>Mathias Seguy</strong>.</br>
+ * <em>http://mathias-seguy.developpez.com/</em></br> </br>
+ * <p/>
+ * *****************************************************************************************************************</br>
+ * Ce code est libre de toute utilisation mais n'est pas distribuable.</br>
+ * Sa distribution est reservée au site <strong>http://android2ee.com</strong>.</br>
+ * Sa propriété intellectuelle appartient à <strong>Mathias Seguy</strong>.</br>
+ * <em>http://mathias-seguy.developpez.com/</em></br> </br>
+ * *****************************************************************************************************************</br>
+ */
+
+package com.android2ee.formation.juilmmxvi.teleric.view.main.main_fragment;
+
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android2ee.formation.juilmmxvi.teleric.R;
+import com.android2ee.formation.juilmmxvi.teleric.model.Human;
+import com.android2ee.formation.juilmmxvi.teleric.view.main.adapter.HumanRecyclerAdapter;
+import com.android2ee.formation.juilmmxvi.teleric.view.main.adapter.OnItemClickedRecyclerView;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+/**
+ * Created by Mathias Seguy - Android2EE on 22/07/2016.
+ */
+public class MainFragment extends Fragment implements OnItemClickedRecyclerView {
+    private static final String TAG = "MainFragment";
+    /***********************************************************
+     * Constant
+     **********************************************************/
+    public static final String EDT = "EDT";
+    public static final String RES = "RES";
+    public static final String MY_DIALOG_ERROR = "MyDialogError";
+    /***********************************************************
+     *  Attributes
+     **********************************************************/
+    /**
+     * The editText to write message
+     */
+    private EditText edtMessage;
+    /**
+     * The button to add the message in  the result area
+     */
+    private Button btnAdd;
+    /**
+     * The Result area
+     */
+    private RecyclerView rcvResult;
+    /**
+     * The arrayAdapter == the model of the list view
+     */
+    private HumanRecyclerAdapter humanRecyclerAdapter;
+    /**
+     * The list of messages to display
+     */
+    private ArrayList<Human> humen;
+    /**
+     * HoneyComb or not  ?o?
+     */
+    private boolean isHoneyComb;
+
+    /**
+     * Animator Set for animation postHC
+     */
+    AnimatorSet animatorSet;
+    /**
+     * Animator Set for animation preHC
+     */
+    Animation animation;
+    /**
+     * The AlertDialog
+     */
+    DeleteDialog dialogFragment;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        isHoneyComb=getResources().getBoolean(R.bool.isHC);
+        setHasOptionsMenu(true);
+    }
+
+    @SuppressLint("NewApi")
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Context ctx=getActivity();
+        View view=inflater.inflate(R.layout.fragment_main,container,false);
+        //instantiate graphical components
+        edtMessage = (EditText) view.findViewById(R.id.edtMessage);
+        btnAdd = (Button) view.findViewById(R.id.btnAdd);
+        rcvResult = (RecyclerView) view.findViewById(R.id.rcvResult);
+        //instanciate animation
+        if(isHoneyComb){
+            animatorSet= (AnimatorSet) AnimatorInflater.loadAnimator(ctx,R.animator.btnadd_pushed_anim);
+            animatorSet.setTarget(btnAdd);
+        }else{
+            animation= AnimationUtils.loadAnimation(ctx,R.anim.btn_add_pushed);
+        }
+        //to avoid selector to be drawn like a big rectancle
+        if (savedInstanceState != null) {
+            humen = savedInstanceState.getParcelableArrayList(RES);
+            edtMessage.setText(savedInstanceState.getString(EDT));
+        } else {
+            humen = new ArrayList<>();
+            for(int i=0;i<2;i++){
+                humen.add(new Human("Item "+i,i));
+            }
+        }
+        humanRecyclerAdapter = new HumanRecyclerAdapter(humen, ctx) {
+            /**
+             * The item is clicked please do something
+             *
+             * @param position
+             */
+            @Override
+            public void itemClicked(int position) {
+                MainFragment.this.itemClicked(position);
+            }
+        };
+        rcvResult.setLayoutManager(new LinearLayoutManager(ctx));
+        rcvResult.setAdapter(humanRecyclerAdapter);
+        //implement the listeners
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMessageInResult();
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(EDT, edtMessage.getText().toString());
+        outState.putParcelableArrayList(RES, humen);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        dialogFragment= (DeleteDialog) getFragmentManager().findFragmentByTag(MY_DIALOG_ERROR);
+        if(dialogFragment!=null){
+            if(dialogFragment.isVisible()){
+                dialogFragment.dismiss();
+            }
+            dialogFragment=null;
+        }
+    }
+
+    /***********************************************************
+     *  Managing menu
+     ****************-******************************************/
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu_deleteall:
+                deleteAll();
+                return true;
+            case R.id.menu_addall:
+                addAll();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /***********************************************************
+     *  Business Methods
+     **********************************************************/
+    String messageTemp;
+    Human humanTemp;
+    /**
+     * Add the message from the editText in the Result area
+     */
+    @SuppressLint("NewApi")
+    private void addMessageInResult() {
+        if(isHoneyComb){
+            animatorSet.start();
+        }else{
+            btnAdd.startAnimation(animation);
+        }
+        messageTemp = edtMessage.getText().toString();
+        //first way
+//        messages.add(message);
+//        arrayAdapter.notifyDataSetChanged();
+        //second
+        humanRecyclerAdapter.add(0,new Human(messageTemp, humen.size()));
+        edtMessage.setText("");
+    }
+
+    /**
+     * Copy the item selected into the editText
+     * @param position
+     */
+    public void itemClicked(int position){
+        Log.e(TAG,"itemClicked called at "+position);
+        messageTemp=humanRecyclerAdapter.getItem(position).getMessage();
+        //showDialog(AlertDialogUniqueID);
+        dialogFragment= (DeleteDialog) getActivity().getSupportFragmentManager().findFragmentByTag(MY_DIALOG_ERROR);
+        if(dialogFragment==null){
+            dialogFragment=new DeleteDialog(this);
+        }
+        dialogFragment.show(getFragmentManager(),"StackDialogDisaplyed");
+    }
+
+    /**
+     * Copythe value of message in the EditText
+     */
+    private void copyMessageInEdt(){
+        edtMessage.setText(messageTemp);
+    }
+
+    /**
+     * Abord copy
+     */
+    private void abordCopy(){
+        Toast.makeText(getActivity(), R.string.toast_abordcopy,Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Delete all the item
+     */
+    public void deleteAll(){
+        humanRecyclerAdapter.deleteAll();
+    }
+
+    /**
+     * Add a bunch of people
+     */
+    public void addAll(){
+        int size= humanRecyclerAdapter.getItemCount();
+        for(int i=size;i<size+2;i++){
+            humen.add(new Human("Item "+i,i));
+        }
+        humanRecyclerAdapter.notifyItemRangeInserted(size,2);
+    }
+
+    /***********************************************************
+     *  AlertDialogFragment pattern
+     **********************************************************/
+    @SuppressLint("ValidFragment")
+    private static class DeleteDialog extends DialogFragment{
+        WeakReference<MainFragment> mainFragmentWeak;
+        public DeleteDialog(MainFragment frag){
+            mainFragmentWeak=new WeakReference<MainFragment>(frag);
+        }
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder alertBulder=new AlertDialog.Builder(getActivity());
+            if(mainFragmentWeak!=null&&mainFragmentWeak.get()!=null){
+                alertBulder.setMessage(getString(R.string.alertdialog_message,mainFragmentWeak.get().messageTemp));
+            }else{
+                alertBulder.setMessage(getString(R.string.alertdialog_message,":("));
+            }
+            alertBulder.setPositiveButton(R.string.alertdialog_ok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(mainFragmentWeak!=null&&mainFragmentWeak.get()!=null){
+                        mainFragmentWeak.get().copyMessageInEdt();
+                    }
+                }
+            });
+            alertBulder.setNegativeButton(R.string.alertdialog_nok, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(mainFragmentWeak!=null&&mainFragmentWeak.get()!=null){
+                        mainFragmentWeak.get().abordCopy();
+                    }
+                }
+            });
+            return alertBulder.create();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            if(mainFragmentWeak!=null&&mainFragmentWeak.get()!=null){
+                ((AlertDialog)getDialog()).setMessage(getString(R.string.alertdialog_message,mainFragmentWeak.get().messageTemp));
+            }
+        }
+    }
+}
