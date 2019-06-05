7165dc022bf9efa68720a91194b0b57ad44a9ca6
MathiasSeguy-Android2EE <mathias.seguy@android2ee.com>
2019-06-05 20:14:29 +0200

    First commit after migration of may 2019

--- app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/generic/MotherActivity.java
+++ app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/generic/MotherActivity.java
@@ -33,9 +33,9 @@ package com.android2ee.formation.juilmmxvi.teleric.view.generic;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
--- app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/main/adapter/HumanRecyclerAdapter.java
+++ app/src/main/java/com/android2ee/formation/juilmmxvi/teleric/view/main/adapter/HumanRecyclerAdapter.java
@@ -32,7 +32,7 @@
 package com.android2ee.formation.juilmmxvi.teleric.view.main.adapter;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
